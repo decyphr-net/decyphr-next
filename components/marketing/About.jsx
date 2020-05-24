cc58737e4d6b63b09e8678d82eafd877d351a673
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-24 15:58:12 -0300

    Update 'why' section

--- components/marketing/About.jsx
+++ components/marketing/About.jsx
@@ -8,17 +8,25 @@ export default function About() {
       <section>
         <div className={styles.leftText}>
           <h2>
-            <FormattedMessage id="Index.about.firstheader" defaultMessage="What does Decyphr do?" /></h2>
+            <FormattedMessage
+              id="Index.about.whatisitheader"
+              defaultMessage="What does Decyphr do?" /></h2>
           <p>
-            <FormattedMessage id="Index.about.firstparagraph" defaultMessage="Decyphr was built to help you translate text from the language that your are learning in to your own native language." />
+            <FormattedMessage
+              id="Index.about.whatisitparagraph1"
+              defaultMessage="Decyphr was built to help you translate text from the language that your are learning in to your own native language." />
           </p>
           
           <p>
-            <FormattedMessage id="Index.about.secondparagraph" defaultMessage="While learning to read a new book you can translate your text and get a breakdown of the structure of your translation, as well as an audio clip so you can also get an idea of what the text should sound like in the original text." />
+            <FormattedMessage
+              id="Index.about.whatisitparagraph2"
+              defaultMessage="While learning to read a new book you can translate your text and get a breakdown of the structure of your translation, as well as an audio clip so you can also get an idea of what the text should sound like in the original text." />
           </p>
 
           <p>
-            <FormattedMessage id="Index.about.thirdparagraph" defaultMessage="Decyphr will take the text that you have translated and generate quizzes for you to allow you to practice, meaning you create the context for your learning from the books that you are reading." />
+            <FormattedMessage
+              id="Index.about.whatisitparagraph3"
+              defaultMessage="Decyphr will take the text that you have translated and generate quizzes for you to allow you to practice, meaning you create the context for your learning from the books that you are reading." />
           </p>
         </div>
         <img className={styles.rightImg} src="/languages.jpg" />
@@ -28,15 +36,19 @@ export default function About() {
         <img className={styles.leftImg} src="/bookstack.jpg" />
         <div className={styles.rightText}>
           <h2>
-            <FormattedMessage id="Index.about.firstheader" defaultMessage="Why Use Decyphr?" /></h2>
+            <FormattedMessage 
+              id="Index.about.whydecyphrheader"
+              defaultMessage="Why Use Decyphr?" /></h2>
           <p>
-            <FormattedMessage id="Index.about.firstparagraph" defaultMessage="Decyphr was built to help you translate text from the language that your are learning in to your own native language." />
+            <FormattedMessage
+              id="Index.about.whydecyphrparagraph1"
+              defaultMessage="Decyphr allows you to choose which books to read and enables you to monitor your process throughout your own personalized library. This will be able to tell how much progress you are making on your journey to becoming fluent in your new language." />
           </p>
           <p>
-            <FormattedMessage id="Index.about.secondparagraph" defaultMessage="While learning to read a new book you can translate your text and get a breakdown of the structure of your translation, as well as an audio clip so you can also get an idea of what the text should sound like in the original text." />
+            <FormattedMessage id="Index.about.whydecyphrparagraph2" defaultMessage="Not only does Decyphr translate the text from your book, but it also helps you to understand the structure of the sentences that you have translated by telling you what words are verbs or nouns, and what tenses a verb belongs to." />
           </p>
           <p>
-            <FormattedMessage id="Index.about.thirdparagraph" defaultMessage="Decyphr will take the text that you have translated and generate quizzes for you to allow you to practice, meaning you create the context for your learning from the books that you are reading." />
+            <FormattedMessage id="Index.about.whydecyphrparagraph3" defaultMessage="Decyphr will quiz you on the phrases that you have translated from your books. This means that, because you are in complete control of the material that you are reading, the material that you will be quizzed on will be tailored to whatever context you choose, giving you a completely personalized learning experience." />
           </p>
         </div>
         
