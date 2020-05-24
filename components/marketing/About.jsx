6ed6db538bb456bfeff1aeb29464c4e7abd296c4
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-23 23:28:11 -0300

    Add additional styling to home page

--- components/marketing/About.jsx
+++ components/marketing/About.jsx
@@ -4,19 +4,44 @@ import styles from './about.module.scss'
 export default function About() {
   return (
     <article className={styles.about}>
-      <h2>
-        <FormattedMessage id="Index.about.firstheader" defaultMessage="What does Decyphr do?" /></h2>
-      <p>
-        <FormattedMessage id="Index.about.firstparagraph" defaultMessage="Decyphr was built to help you translate text from the language that your are learning in to your own native language." />
-      </p>
-      
-      <p>
-        <FormattedMessage id="Index.about.secondparagraph" defaultMessage="While learning to read a new book you can translate your text and get a breakdown of the structure of your translation, as well as an audio clip so you can also get an idea of what the text should sound like in the original text." />
-      </p>
 
-      <p>
-        <FormattedMessage id="Index.about.thirdparagraph" defaultMessage="Decyphr will take the text that you have translated and generate quizzes for you to allow you to practice, meaning you create the context for your learning from the books that you are reading." />
-      </p>
+      <section>
+        <div className={styles.leftText}>
+          <h2>
+            <FormattedMessage id="Index.about.firstheader" defaultMessage="What does Decyphr do?" /></h2>
+          <p>
+            <FormattedMessage id="Index.about.firstparagraph" defaultMessage="Decyphr was built to help you translate text from the language that your are learning in to your own native language." />
+          </p>
+          
+          <p>
+            <FormattedMessage id="Index.about.secondparagraph" defaultMessage="While learning to read a new book you can translate your text and get a breakdown of the structure of your translation, as well as an audio clip so you can also get an idea of what the text should sound like in the original text." />
+          </p>
+
+          <p>
+            <FormattedMessage id="Index.about.thirdparagraph" defaultMessage="Decyphr will take the text that you have translated and generate quizzes for you to allow you to practice, meaning you create the context for your learning from the books that you are reading." />
+          </p>
+        </div>
+        <img className={styles.rightImg} src="/languages.jpg" />
+      </section>
+
+      <section>
+        <img className={styles.leftImg} src="/bookstack.jpg" />
+        <div className={styles.rightText}>
+          <h2>
+            <FormattedMessage id="Index.about.firstheader" defaultMessage="Why Use Decyphr?" /></h2>
+          <p>
+            <FormattedMessage id="Index.about.firstparagraph" defaultMessage="Decyphr was built to help you translate text from the language that your are learning in to your own native language." />
+          </p>
+          <p>
+            <FormattedMessage id="Index.about.secondparagraph" defaultMessage="While learning to read a new book you can translate your text and get a breakdown of the structure of your translation, as well as an audio clip so you can also get an idea of what the text should sound like in the original text." />
+          </p>
+          <p>
+            <FormattedMessage id="Index.about.thirdparagraph" defaultMessage="Decyphr will take the text that you have translated and generate quizzes for you to allow you to practice, meaning you create the context for your learning from the books that you are reading." />
+          </p>
+        </div>
+        
+      </section>
+      
     </article>
   )
 }
