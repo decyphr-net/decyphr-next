96d7e947475ff3d446fc9e67533520294ccba582
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-25 19:28:37 -0300

    Add some translations for login page

--- pages/accounts/login.jsx
+++ pages/accounts/login.jsx
@@ -1,13 +1,30 @@
+import { FormattedMessage } from 'react-intl'
 import styles from './login.module.scss'
 
 export default function Login() {
   return (
     <main className={styles.main}>
       <section className={styles.leftPanel}>
-        <h1>Welcome to Decyphr</h1>
-        <p>Just login to get back to your learning.</p>
-        <p>If you don't have an account yet then you can sign up for one here!</p>
-        <p>Or, if you have forgotten your password, click here!</p>
+        <h1>
+          <FormattedMessage
+            id="Accounts.login.leftpanel.header"
+            defaultMessage="Welcome to Decyphr" />
+        </h1>
+        <p>
+          <FormattedMessage
+            id="Accounts.login.leftpanel.helpparagraph"
+            defaultMessage="Just login to get back to your learning." />
+          </p>
+        <p>
+          <FormattedMessage
+            id="Accounts.login.leftpanel.signupprompt"
+            defaultMessage="If you don't have an account yet then you can sign up for one here!" />
+          </p>
+        <p>
+          <FormattedMessage
+            id="Accounts.login.leftPanel.forgotpasswordprompt"
+            defaultMessage="Or, if you have forgotten your password, click here!" />
+        </p>
       </section>
 
       <section className={styles.rightPanel}>
