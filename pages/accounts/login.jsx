f1620ba60ff31790116f1220857aebd19487f188
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-24 21:15:04 -0300

    Add basic login form design

--- pages/accounts/login.jsx
+++ pages/accounts/login.jsx
@@ -1,5 +1,4 @@
 import styles from './login.module.scss'
-import {TextField} from '../../components/elements/Input'
 
 export default function Login() {
   return (
@@ -8,10 +7,18 @@ export default function Login() {
         <h1>Welcome to Decyphr</h1>
         <p>Just login to get back to your learning.</p>
         <p>If you don't have an account yet then you can sign up for one here!</p>
+        <p>Or, if you have forgotten your password, click here!</p>
       </section>
 
       <section className={styles.rightPanel}>
-        <TextField />
+        <h2>Login To Decyphr</h2>
+        <form>
+          
+          <input className={styles.formInput} placeholder="Email" name="email" type="text" />
+          <input className={styles.formInput} placeholder="Password" name="password" type="password" />
+          <button className={styles.formButton}>Login!</button>
+        </form>
+        
       </section>
     </main>
   )
