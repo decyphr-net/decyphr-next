7a6c33349a964ce1a130fcf4bcefb6b3676de858
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-27 10:42:07 -0300

    Add head to login and tidy up

--- pages/accounts/login.jsx
+++ pages/accounts/login.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-import { renderToString } from 'react-dom/server'
 import { FormattedMessage } from 'react-intl'
 import Router from 'next/router'
+import Head from 'next/head'
 import styles from './login.module.scss'
 import api from '../../utils/api'
 
@@ -48,66 +48,73 @@ export default function Login() {
   }
 
   return (
-    <main className={styles.main}>
-      <section className={styles.leftPanel}>
-        <h1>
-          <FormattedMessage
-            id="Accounts.login.leftpanel.header"
-            defaultMessage="Welcome to Decyphr" />
-        </h1>
-        <p>
-          <FormattedMessage
-            id="Accounts.login.leftpanel.helpparagraph"
-            defaultMessage="Just login to get back to your learning." />
-          </p>
-        <p>
-          <FormattedMessage
-            id="Accounts.login.leftpanel.signupprompt"
-            defaultMessage="If you don't have an account yet then you can sign up for one here!" />
-          </p>
-        <p>
-          <FormattedMessage
-            id="Accounts.login.leftPanel.forgotpasswordprompt"
-            defaultMessage="Or, if you have forgotten your password, click here!" />
-        </p>
-      </section>
-
-      <section className={styles.rightPanel}>
-        <h2>
-          <FormattedMessage
-            id="Accounts.login.rightpanel.header"
-            defaultMessage="Login To Decyphr" />
-          </h2>
-
-        {errors.length > 0 &&
-          <p>{errors}</p>
-        }
-
-        <form>
-          <FormattedMessage id="Accounts.login.rightpanel.emailfield" defaultMessage="Email">
-            {
-              placeholder => (
-                <input className={styles.formInput} placeholder={placeholder} name="email" type="text" onChange={e => setEmail(e.target.value)} />
-              )
-            }
-          </FormattedMessage>
-
-          <FormattedMessage id="Accounts.login.rightpanel.passwordfield" defaultMessage="Password">
-            {
-              placeholder => (
-                <input className={styles.formInput} placeholder={placeholder} name="password" type="password" onChange={e => setPassword(e.target.value)} />
-              )
-            }
-          </FormattedMessage>
-
-          <button className={styles.formButton} onClick={e => submitForm(e)}>
+    <>
+      <Head>
+        <title>Log into Decyphr!</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"></link>
+      </Head>
+      <main className={styles.main}>
+        <section className={styles.leftPanel}>
+          <h1>
             <FormattedMessage
-              id="Accounts.login.rightpanel.loginbutton"
-              defaultMessage="Login!" />
-          </button>
-        </form>
-        
-      </section>
-    </main>
+              id="Accounts.login.leftpanel.header"
+              defaultMessage="Welcome to Decyphr" />
+          </h1>
+          <p>
+            <FormattedMessage
+              id="Accounts.login.leftpanel.helpparagraph"
+              defaultMessage="Just login to get back to your learning." />
+            </p>
+          <p>
+            <FormattedMessage
+              id="Accounts.login.leftpanel.signupprompt"
+              defaultMessage="If you don't have an account yet then you can sign up for one here!" />
+            </p>
+          <p>
+            <FormattedMessage
+              id="Accounts.login.leftPanel.forgotpasswordprompt"
+              defaultMessage="Or, if you have forgotten your password, click here!" />
+          </p>
+        </section>
+
+        <section className={styles.rightPanel}>
+          <h2>
+            <FormattedMessage
+              id="Accounts.login.rightpanel.header"
+              defaultMessage="Login To Decyphr" />
+            </h2>
+
+          {errors.length > 0 &&
+            <p>{errors}</p>
+          }
+
+          <form>
+            <FormattedMessage id="Accounts.login.rightpanel.emailfield" defaultMessage="Email">
+              {
+                placeholder => (
+                  <input className={styles.formInput} placeholder={placeholder} name="email" type="text" onChange={e => setEmail(e.target.value)} />
+                )
+              }
+            </FormattedMessage>
+
+            <FormattedMessage id="Accounts.login.rightpanel.passwordfield" defaultMessage="Password">
+              {
+                placeholder => (
+                  <input className={styles.formInput} placeholder={placeholder} name="password" type="password" onChange={e => setPassword(e.target.value)} />
+                )
+              }
+            </FormattedMessage>
+
+            <button className={styles.formButton} onClick={e => submitForm(e)}>
+              <FormattedMessage
+                id="Accounts.login.rightpanel.loginbutton"
+                defaultMessage="Login!" />
+            </button>
+          </form>
+          
+        </section>
+      </main>
+    </>
   )
 }
