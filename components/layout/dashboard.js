ab89ce8151b2dcf8aa935eb588231b2215f0aa59
Aaron Sinnott <aaronsnig@gmail.com>
2020-05-31 13:08:28 -0300

    Implement dashboard layout

---
+++ components/layout/dashboard.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
+import Navigation from '../../components/layout/Navigation'
+import styles from './dashboard.module.scss'
+
+export default function Library({ children, pageTitle, pageSubtitle }) {
+
+  useEffect(() => {
+    if (!window.localStorage.getItem('token')) {
+      Router.push('/accounts/login')
+    }
+  })
+
+  return (
+    <main className={styles.layout}>
+      <Navigation />
+      <section>
+        <h1 className={styles.pageTitle}>Library</h1>
+        <p className={styles.pageSubtitle}>{pageSubtitle}</p>
+      </section>
+
+      <section>
+        {children}
+      </section>
+    </main>
+  )
+}
