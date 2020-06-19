5118345a52f602475d26a5034ce0ba88375ab4b2
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-19 14:56:33 -0300

    Squashed commit of the following:
    commit 006ab7d457365510087b6f223c17af6955dce5a7
    Author: Aaron Sinnott <aaronsnig@gmail.com>
    Date:   Fri Jun 19 14:52:22 2020 -0300
    
        Update reading-session title
    
    commit 55e520434a6693aaf85ef397d0aa9ef536664060
    Author: Aaron Sinnott <aaronsnig@gmail.com>
    Date:   Fri Jun 19 14:52:04 2020 -0300
    
        Update library title
    
    commit 85c4cb34e76d16f45626ba7ff1e789a62bced06f
    Author: Aaron Sinnott <aaronsnig@gmail.com>
    Date:   Fri Jun 19 14:49:43 2020 -0300
    
        Update dashboard to use dashboard layout
    
    commit d7ccc45b874dfa24e1e9c8c581f27ad864ea3344
    Author: Aaron Sinnott <aaronsnig@gmail.com>
    Date:   Fri Jun 19 14:49:26 2020 -0300
    
        Update dashboard to include the title prop

--- components/layout/dashboard.js
+++ components/layout/dashboard.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 import Router from 'next/router'
+import Head from 'next/head'
 import { Row, Col } from 'react-bootstrap'
 import Navigation from '../../components/layout/Navigation'
 import styles from './dashboard.module.scss'
 
-export default function Library({ children, pageTitle, pageSubtitle }) {
+export default function Library({ children, title, pageTitle, pageSubtitle }) {
 
   useEffect(() => {
     if (!window.localStorage.getItem('token')) {
@@ -13,23 +14,28 @@ export default function Library({ children, pageTitle, pageSubtitle }) {
   })
 
   return (
-    <main className={styles.layout}>
-      <Navigation />
-      <section>
-        <Row noGutters={true}>
-          <Col xs={{ span: 11, offset: 1 }}>
-            <h1 className={styles.pageTitle}>{pageTitle}</h1>
-          </Col>
+    <>
+      <Head>
+        <title>{ title }</title>
+      </Head>
+      <main className={styles.layout}>
+        <Navigation />
+        <section>
+          <Row noGutters={true}>
+            <Col xs={{ span: 11, offset: 1 }}>
+              <h1 className={styles.pageTitle}>{pageTitle}</h1>
+            </Col>
 
-          <Col xs={{ span: 11, offset: 1 }}>
-            <p className={styles.pageSubtitle}>{pageSubtitle}</p>
-          </Col>
-        </Row>
-      </section>
+            <Col xs={{ span: 11, offset: 1 }}>
+              <p className={styles.pageSubtitle}>{pageSubtitle}</p>
+            </Col>
+          </Row>
+        </section>
 
-      <section>
-        {children}
-      </section>
-    </main>
+        <section>
+          {children}
+        </section>
+      </main>
+    </>
   )
 }
