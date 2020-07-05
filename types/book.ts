49c341dcbcd27c9c996d0335af326afe02ba5ff9
Aaron Sinnott <aaronsnig@gmail.com>
2020-07-05 00:24:07 -0300

    Re-implement some of the basic items for translations

---
+++ types/book.ts
@@ -0,0 +1,12 @@
+export default interface Book {
+  author: string
+  category: string
+  description: string
+  id: number
+  language: number
+  publish_date: string
+  publisher: string
+  small_thumbnail: string
+  thumbnail: string
+  title: string
+}
