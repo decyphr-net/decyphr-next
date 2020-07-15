6aed7f97d5937c02842d8653e3e5b5cf08dc58ad
Aaron Sinnott <aaronsnig@gmail.com>
2020-07-15 15:13:52 -0300

    Fill out the contents the dashboard page (#43)

--- pages/[lang]/dashboard/index.tsx
+++ pages/[lang]/dashboard/index.tsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react'
 import Router from 'next/router'
+import { Card, Row, Col } from 'react-bootstrap'
 import withLocale from '../../../i18n/hoc/withLocale'
 import useTranslation from '../../../i18n/hooks/useTranslation'
-import api from '../../../utils/api'
+import APIInterface from '../../../utils/api/client'
 import DashboardLayout from '../../../components/layout/dashboard'
+import { Button } from '../../../components/elements/Button'
 
 const Dashboard: React.FC = () => {
   const { locale, t } = useTranslation()
-  const [translations, setTranslations] = useState([]);
-  const [libraryItems, setLibraryItems] = useState([]);
-  const [readingSessions, setReadingSessions] = useState([]);
-  const [practiceSessions, setPracticeSessions] = useState([]);
-  const [errors, setErrors] = useState([]);
+
+  const [translationCount, setTranslationCount] = useState(0)
+  const [libraryItemCount, setlibraryItemCount] = useState(0)
+  const [readingSessionCount, setReadingSessionCount] = useState(0)
+  const [practiceSessionCount, setPracticeSessionCount] = useState(0)
+
+  const [errors, setErrors] = useState([])
 
   useEffect(() => {
     if (!window.localStorage.getItem('token')) {
@@ -23,35 +27,64 @@ const Dashboard: React.FC = () => {
     getDashboardData()
   }, [])
 
-  const setStateItems = async (response) => {
-    await setTranslations(response.translations)
-    await setLibraryItems(response.library_items)
-    await setReadingSessions(response.reading_sessions)
-    await setPracticeSessions(response.practice_sessions)
+  const getDashboardData = async () => {
+    let apiInterface = new APIInterface('dashboard')
+    apiInterface.request()
+      .then(response => {
+        setTranslationCount(response.translations_count)
+        setlibraryItemCount(response.library_item_count)
+        setPracticeSessionCount(response.practice_sessions_count)
+        setReadingSessionCount(response.reading_sessions_count)
 
-    localStorage.setItem('library', JSON.stringify(response.library_items))
-    localStorage.setItem('translations', JSON.stringify(response.library_items))
-    localStorage.setItem('readingsessions', JSON.stringify(response.reading_sessions))
-    localStorage.setItem('practicesessions', JSON.stringify(response.practice_sessions))
+        // We need to set the translation count to determine whether or not a
+        // user can access the practice sessions.
+        localStorage.setItem('translationCount', response.translations_count)
+      })
+      .catch(errors => setErrors(errors))
   }
 
-  const getDashboardData = async () => {
-    if (localStorage.getItem('library')
-        && localStorage.getItem('translations') 
-        && localStorage.getItem('readingsessions') 
-        && localStorage.getItem('practicesessions')) 
-    {
-      await setTranslations(JSON.parse(localStorage.getItem('library')))
-      await setLibraryItems(JSON.parse(localStorage.getItem('translations')))
-      await setReadingSessions(JSON.parse(localStorage.getItem('readingsessions')))
-      await setPracticeSessions(JSON.parse(localStorage.getItem('practicesessions')))
-    } else {
-      await api('GET', 'dashboard', setStateItems, setErrors, true)
-    }
+  const redirectToPage = e => {
+    let pageName = e.target.value
+    Router.push(`/[lang]/${pageName}`, `/${locale}/${pageName}`)
   }
 
   return (
     <DashboardLayout title="Decyphr Dashboard" pageTitle="Dashboard" pageSubtitle="Welcome to your Decyphr Dashboard">
+      <Row noGutters={true} className="justify-content-md-center">
+        <Col className="text-center" sm={12} md={3}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Translations</Card.Title>
+              <Card.Text>You have translated</Card.Text>
+              <Card.Text>{translationCount}</Card.Text>
+              <Card.Text>pieces of text</Card.Text>
+              <Button 
+                className={"text-sm-center text-md-right"}
+                text={"Learn new words!"}
+                value={"library"}
+                onClickHandler={redirectToPage}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="text-center" sm={12} md={3}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Practice Sessions</Card.Title>
+              <Card.Text>You have completed</Card.Text>
+              <Card.Text>{practiceSessionCount}</Card.Text>
+              <Card.Text>sessions to date</Card.Text>
+              <Button 
+                className={"text-sm-center text-md-right"}
+                text={"Reinforce those words!"}
+                value={"practice"}
+                onClickHandler={redirectToPage}
+              />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      
     </DashboardLayout>
   )
 }
