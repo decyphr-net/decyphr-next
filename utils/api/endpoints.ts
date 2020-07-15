e592493183e38fbeb6d3dfdd4658aae00e37ca81
Aaron Sinnott <aaronsnig@gmail.com>
2020-07-15 14:43:35 -0300

    Implement a new API wrapper to tidy up code

---
+++ utils/api/endpoints.ts
@@ -0,0 +1,52 @@
+const endpoints = {
+  login: {
+    url: 'users/login/',
+    method: 'POST'
+  },
+  register: {
+    url: 'users/register/',
+    method: 'POST'
+  },
+  languages: {
+    url: 'languages/',
+    method: 'GET'
+  },
+  dashboard: {
+    url: 'dashboard/',
+    method: 'GET'
+  },
+  bookSearch: {
+    url: 'books/?name=',
+    method: 'GET'
+  },
+  getReadingList: {
+    url: 'reading-list/',
+    method: 'GET',
+  },
+  addToReadingList: {
+    url: 'reading-list/',
+    method: 'POST',
+  },
+  createReadingSession: {
+    url: 'reading-sessions/',
+    method: 'POST'
+  },
+  translate: {
+    url: 'translate/',
+    method: 'POST'
+  },
+  createPracticeSession: {
+    url: 'practice-sessions/sessions/',
+    method: 'POST'
+  },
+  updatePracticeSession: {
+    url: 'practice-sessions/sessions/',
+    method: 'PUT'
+  },
+  questions: {
+    url: 'practice-sessions/questions/',
+    method: 'PUT'
+  }
+}
+
+export default endpoints
